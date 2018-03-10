9e6479796ad199979996fd6553e9472a6bee6add
Johannes <johnawesomo@yahoo.de>
2018-03-10 19:07:32 +0100

    finish OAuth flow

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,8 +13,20 @@ module.exports = app => {
   // this route includes the code coming from google after granting access
   app.get('/auth/google/callback', passport.authenticate('google'));
 
+  app.get('/auth/logout', (req, res) => {
+    req.logout();
+    res.send({
+      responseMessage: 'User has succesfully logged out: ',
+      user: req.user
+    });
+  });
+
+  app.get('/auth/current/session', (req, res) => {
+    res.send(req.session);
+  });
+
   // test authentication flow with cookies
-  app.get('/auth/google/current/user', (req, res) => {
+  app.get('/auth/current/user', (req, res) => {
     res.send(req.user);
   });
 };
